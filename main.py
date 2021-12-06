6aee633078175b55133d70cab71da55cbde14bed
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-06 16:28:50 -0500

    working still in progress

---
+++ main.py
@@ -0,0 +1,42 @@
+first_name = input("Enter first_name:")
+last_name = input("Enter last_name:")
+print(f"Hello {first_name} {last_name}")
+
+# Create personalize welcome user Vacation Budget Planner
+Sabrina_Pope = input("Welcome to the Vacation Budget Planner, Where would you like to travel:")
+
+# Ask the user where would the like to Vacation 
+print("Jamaican_trip")
+
+# Ask the user how many days would you like to spend Jamaica
+Jamaican_trip = input("Enter the amount of days on trip: ")
+print = (5)
+
+# Ask the user how much money to bring on trip 
+input_Spending_Money = input("How much money I would like to bring: ")
+Spending_Money = ("Budget_per_day: 100")
+
+# Ask the user how much would they like to spend per day
+input_Spending_Money = input("How much do I need to spending in a day: ")
+
+#Ask much in USD monies do you plan to spend on trip
+float = (input(("Budget_per_day 100 USD to JMD = $15528.00, JMD = 0.64 USD, 100 / 5, Spending_Money, number_of_people, 1")))
+
+#Create a while loop for Vacation Budget Planner user
+jamaican_trip = True
+
+while jamaican_trip: 
+   first_name = str(input("Enter first_name"))
+   last_name = str(input("Enter last_name"))
+   jamaican_trip = str(input("Enter jamaica for 5 days with 100.00 for the week:"))
+   jamaican_trip = str(input("Enter you will have 20.00 per day for spending money:"))
+   
+   print(first_name + last_name + jamaica for 5 days with 100.00 for the week + you will have 20.00 per day for spending money:)
+ 
+   repeat = input("try again: yes or no")
+
+   if repeat = != "yes"
+
+    jamaican_trip = False
+    
+   
