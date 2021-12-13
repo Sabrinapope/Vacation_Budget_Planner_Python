8dcf48ea11a97dc652eb35ce26191cdf8a786ec4
Sabrinapope.md <40004402+Sabrinapope@users.noreply.github.com>
2021-12-12 23:03:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,30 @@ Sabrina_Pope = input("Welcome to the Vacation Budget Planner, Where would you li
 # Ask the user where would the like to Vacation 
 print("Jamaican_trip")
 
-# Ask the user how many days would you like to spend Jamaica
+# Print a line for for formatting purposes and 
+print (f"a line for formatting ie:**************")
+
+#ask the user how many days would you like to spend Jamaica
 Jamaican_trip = input("Enter the amount of days on trip: ")
 print = (5)
 
 # Ask the user how much money to bring on trip 
 input_Spending_Money = input("How much money I would like to bring: ")
-Spending_Money = ("Budget_per_day: 100")
+Spending_Money = ("Budget_per_day: 100. 0 USD ")
 
 # Ask the user how much would they like to spend per day
-input_Spending_Money = input("How much do I need to spending in a day: ")
+input_Spending_Money = input("How much do I need to spending in a day: 20 ")
 
 #Ask much in USD monies do you plan to spend on trip
 float = (input(("Budget_per_day 100 USD to JMD = $15528.00, JMD = 0.64 USD, 100 / 5, Spending_Money, number_of_people, 1")))
 
-#Create a while loop for Vacation Budget Planner user
+#Calculate the number of days hours, and minutes on jamaica trip
+total_hours = "105.0 hr"
+total_minutes = "6,300 mins:"
+total_for_week = "4 days 9 hr:"
+float = (input("hours for week, 105" "minutes for the week, 6,300 "  +  "numberof days, 4 days and 9 hours "))
+
+#Create a while loop for Vacation Budget user
 jamaican_trip = True
 
 while jamaican_trip: 
@@ -31,12 +40,12 @@ while jamaican_trip:
    jamaican_trip = str(input("Enter jamaica for 5 days with 100.00 for the week:"))
    jamaican_trip = str(input("Enter you will have 20.00 per day for spending money:"))
    
-   print(first_name + last_name + jamaica for 5 days with 100.00 for the week + you will have 20.00 per day for spending money:)
+   print(f"{first_name}  {last_name}  jamaica for 5 days with 100.00 for the week you will have 20.00 per day for spending money:")
  
    repeat = input("try again: yes or no")
 
-   if repeat = != "yes"
-
+   if repeat  != "yes":
+       
     jamaican_trip = False
     
    
